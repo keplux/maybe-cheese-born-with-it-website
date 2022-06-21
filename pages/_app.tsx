9d994a6b178113b9a97877ec5b377563d02e7e22
Chris Jardine <dev@keplux.com>
2022-06-20 21:49:31 -0400

    Move GA script to proper file.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,24 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Footer, Navbar } from '../src';
 import Image from 'next/image';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className='bg-brand-900'>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+        strategy='afterInteractive'
+      />
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+        `}
+      </Script>
       <div className='fixed w-screen h-screen'>
         <Image
           src='/images/get-up-on-outta-here-with-my-cheeseholes.svg'
