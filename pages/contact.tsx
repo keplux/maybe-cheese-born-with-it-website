c1a73bcbee45d6cf915b62e084657f5c44ffe949
Chris Jardine <chris@keplux.com>
2024-09-02 12:43:03 -0400

    Update booking email.

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,10 +1,10 @@
-import { zodResolver } from '@hookform/resolvers/zod';
-import { NextSeo } from 'next-seo';
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { NextSeo } from "next-seo";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 const inputStyle =
-  'py-3 px-4 block w-full shadow-sm hover:border-yellow-500 focus:ring-yellow-500 focus:border-yellow-500 transition duration-200 caret-yellow-500 border border-black rounded-md';
+  "py-3 px-4 block w-full shadow-sm hover:border-yellow-500 focus:ring-yellow-500 focus:border-yellow-500 transition duration-200 caret-yellow-500 border border-black rounded-md";
 
 type ContactFormData = {
   firstName: string;
@@ -15,11 +15,11 @@ type ContactFormData = {
 };
 
 const resolver = z.object({
-  firstName: z.string().min(1, 'Required'),
+  firstName: z.string().min(1, "Required"),
   lastName: z.string(),
-  email: z.string().min(1, 'Required').email('Invalid email'),
+  email: z.string().min(1, "Required").email("Invalid email"),
   phone: z.string(),
-  message: z.string().min(32, 'Please add more details'),
+  message: z.string().min(32, "Please add more details"),
 });
 
 const Contact = () => {
@@ -30,19 +30,19 @@ const Contact = () => {
     formState: { isSubmitting, errors },
   } = useForm<ContactFormData>({
     defaultValues: {
-      firstName: '',
-      lastName: '',
-      email: '',
-      phone: '',
-      message: '',
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      message: "",
     },
     resolver: zodResolver(resolver),
   });
 
   const onSubmit = async (data: ContactFormData) => {
-    const res = await fetch('/api/submitContactForm', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+    const res = await fetch("/api/submitContactForm", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         firstName: data.firstName,
         lastName: data.lastName,
@@ -61,180 +61,180 @@ const Contact = () => {
     <div>
       <NextSeo
         title="Planning an Event? We'll Bring the Cheese! | Toledo, OH"
-        description='Bring the sassiest food truck in toledo to your next event! Your guests will love our gooey mac and cheese truck! Book us or inquire today: (419) 245-8056.'
-        canonical='https://www.maybecheesebornwithit.com/contact'
+        description="Bring the sassiest food truck in toledo to your next event! Your guests will love our gooey mac and cheese truck! Book us or inquire today: (419) 245-8056."
+        canonical="https://www.maybecheesebornwithit.com/contact"
       />
-      <div className='relative py-16'>
-        <div className='px-2'>
-          <div className='py-16 px-4 max-w-3xl sm:px-6 lg:px-8 bg-white mx-auto rounded-t-lg overflow-hidden shadow-lg border-x-2 border-t-2 border-black'>
-            <div className='relative max-w-xl mx-auto'>
-              <div className='flex justify-center text-center'>
-                <h1 className='font-display text-yellow-500 text-4xl sm:text-6xl font-extrabold [text-shadow:_-2px_2px_0px_#CE5937]'>
+      <div className="relative py-16">
+        <div className="px-2">
+          <div className="py-16 px-4 max-w-3xl sm:px-6 lg:px-8 bg-white mx-auto rounded-t-lg overflow-hidden shadow-lg border-x-2 border-t-2 border-black">
+            <div className="relative max-w-xl mx-auto">
+              <div className="flex justify-center text-center">
+                <h1 className="font-display text-yellow-500 text-4xl sm:text-6xl font-extrabold [text-shadow:_-2px_2px_0px_#CE5937]">
                   CONTACT US
                 </h1>
               </div>
-              <div className='font-handwriting text-xl font-bold text-center mt-8'>
+              <div className="font-handwriting text-xl font-bold text-center mt-8">
                 We want to hear from you!
               </div>
             </div>
           </div>
         </div>
-        <div className='py-16 bg-yellow-400 border-y-2 border-black'>
-          <div className='max-w-3xl mx-auto px-8'>
-            <h2 className='font-bold'>Inquiries/Booking</h2>
-            <div className='flex items-center mt-1'>
+        <div className="py-16 bg-yellow-400 border-y-2 border-black">
+          <div className="max-w-3xl mx-auto px-8">
+            <h2 className="font-bold">Inquiries/Booking</h2>
+            <div className="flex items-center mt-1">
               <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='h-5 w-5 text-black'
-                viewBox='0 0 20 20'
-                fill='currentColor'
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 text-black"
+                viewBox="0 0 20 20"
+                fill="currentColor"
               >
-                <path d='M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z' />
+                <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
               </svg>
-              <a href='tel:+14192458056'>
-                <span className='ml-2 text-gray-800 text-sm hover:text-brand-600'>
+              <a href="tel:+14192458056">
+                <span className="ml-2 text-gray-800 text-sm hover:text-brand-600">
                   (419) 245-8056
                 </span>
               </a>
             </div>
-            <div className='flex items-center mt-2'>
+            <div className="flex items-center mt-2">
               <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='h-5 w-5 text-black'
-                viewBox='0 0 20 20'
-                fill='currentColor'
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 text-black"
+                viewBox="0 0 20 20"
+                fill="currentColor"
               >
                 <path
-                  fillRule='evenodd'
-                  d='M14.243 5.757a6 6 0 10-.986 9.284 1 1 0 111.087 1.678A8 8 0 1118 10a3 3 0 01-4.8 2.401A4 4 0 1114 10a1 1 0 102 0c0-1.537-.586-3.07-1.757-4.243zM12 10a2 2 0 10-4 0 2 2 0 004 0z'
-                  clipRule='evenodd'
+                  fillRule="evenodd"
+                  d="M14.243 5.757a6 6 0 10-.986 9.284 1 1 0 111.087 1.678A8 8 0 1118 10a3 3 0 01-4.8 2.401A4 4 0 1114 10a1 1 0 102 0c0-1.537-.586-3.07-1.757-4.243zM12 10a2 2 0 10-4 0 2 2 0 004 0z"
+                  clipRule="evenodd"
                 />
               </svg>
-              <a href='mailto:booking@maybecheesebornwithit.com'>
-                <span className='ml-2 text-gray-800 text-sm hover:text-brand-600'>
-                  booking@maybecheesebornwithit.com
+              <a href="mailto:booking@maybecheesebornwithit.org">
+                <span className="ml-2 text-gray-800 text-sm hover:text-brand-600">
+                  booking@maybecheesebornwithit.org
                 </span>
               </a>
             </div>
           </div>
         </div>
-        <div className='px-2'>
-          <div className='py-16 px-4 max-w-3xl sm:px-6 lg:px-8 bg-white mx-auto rounded-b-lg overflow-hidden shadow-lg border-x-2 border-b-2 border-black'>
+        <div className="px-2">
+          <div className="py-16 px-4 max-w-3xl sm:px-6 lg:px-8 bg-white mx-auto rounded-b-lg overflow-hidden shadow-lg border-x-2 border-b-2 border-black">
             <form
               onSubmit={handleSubmit(onSubmit)}
-              method='POST'
-              className='grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8'
+              method="POST"
+              className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
             >
               <div>
-                <label htmlFor='firstName' className='block text-brand-600'>
+                <label htmlFor="firstName" className="block text-brand-600">
                   First name
                 </label>
-                <div className='mt-1'>
+                <div className="mt-1">
                   <input
-                    type='text'
-                    autoComplete='given-name'
+                    type="text"
+                    autoComplete="given-name"
                     className={`${inputStyle} ${
-                      errors.firstName && 'border-red-500'
+                      errors.firstName && "border-red-500"
                     }`}
-                    {...register('firstName')}
+                    {...register("firstName")}
                   />
                   {errors.firstName && (
-                    <p className='mt-1 text-xs text-red-500 italic'>
+                    <p className="mt-1 text-xs text-red-500 italic">
                       {errors.firstName.message}
                     </p>
                   )}
                 </div>
               </div>
               <div>
-                <label htmlFor='lastName' className='block text-brand-600'>
+                <label htmlFor="lastName" className="block text-brand-600">
                   Last name
                 </label>
-                <div className='mt-1'>
+                <div className="mt-1">
                   <input
-                    type='text'
-                    autoComplete='family-name'
+                    type="text"
+                    autoComplete="family-name"
                     className={`${inputStyle} ${
-                      errors.lastName && 'border-red-500'
+                      errors.lastName && "border-red-500"
                     }`}
-                    {...register('lastName')}
+                    {...register("lastName")}
                   />
                   {errors.lastName && (
-                    <p className='mt-1 text-xs text-red-500 italic'>
+                    <p className="mt-1 text-xs text-red-500 italic">
                       {errors.lastName.message}
                     </p>
                   )}
                 </div>
               </div>
-              <div className='sm:col-span-2'>
-                <label htmlFor='email' className='block text-brand-600'>
+              <div className="sm:col-span-2">
+                <label htmlFor="email" className="block text-brand-600">
                   Email
                 </label>
-                <div className='mt-1'>
+                <div className="mt-1">
                   <input
-                    type='email'
-                    autoComplete='email'
+                    type="email"
+                    autoComplete="email"
                     className={`${inputStyle} ${
-                      errors.email && 'border-red-500'
+                      errors.email && "border-red-500"
                     }`}
-                    {...register('email')}
+                    {...register("email")}
                   />
                   {errors.email && (
-                    <p className='mt-1 text-xs text-red-500 italic'>
+                    <p className="mt-1 text-xs text-red-500 italic">
                       {errors.email.message}
                     </p>
                   )}
                 </div>
               </div>
-              <div className='sm:col-span-2'>
-                <label htmlFor='phone' className='block text-brand-600'>
+              <div className="sm:col-span-2">
+                <label htmlFor="phone" className="block text-brand-600">
                   Phone number
                 </label>
-                <div className='mt-1'>
+                <div className="mt-1">
                   <input
-                    type='tel'
-                    autoComplete='phone'
+                    type="tel"
+                    autoComplete="phone"
                     className={`${inputStyle} ${
-                      errors.phone && 'border-red-500'
+                      errors.phone && "border-red-500"
                     }`}
-                    {...register('phone')}
+                    {...register("phone")}
                   />
                   {errors.phone && (
-                    <p className='mt-1 text-xs text-red-500 italic'>
+                    <p className="mt-1 text-xs text-red-500 italic">
                       {errors.phone.message}
                     </p>
                   )}
                 </div>
               </div>
-              <div className='sm:col-span-2'>
-                <label htmlFor='message' className='block text-brand-600'>
+              <div className="sm:col-span-2">
+                <label htmlFor="message" className="block text-brand-600">
                   Message
                 </label>
-                <div className='mt-1'>
+                <div className="mt-1">
                   <textarea
                     rows={4}
                     className={`${inputStyle} ${
-                      errors.message && 'border-red-500'
+                      errors.message && "border-red-500"
                     }`}
-                    {...register('message')}
+                    {...register("message")}
                   />
                   {errors.message && (
-                    <p className='mt-1 text-xs text-red-500 italic'>
+                    <p className="mt-1 text-xs text-red-500 italic">
                       {errors.message.message}
                     </p>
                   )}
                 </div>
               </div>
-              <div className='sm:col-span-2'>
+              <div className="sm:col-span-2">
                 <button
-                  type='submit'
+                  type="submit"
                   disabled={isSubmitting}
                   className={`cursor-pointer mt-8 flex justify-center w-full items-center px-8 py-2 text-xl rounded-lg shadow-md font-semibold transition ${
                     isSubmitting
-                      ? 'bg-gray-400'
-                      : 'bg-yellow-400 hover:bg-yellow-500'
+                      ? "bg-gray-400"
+                      : "bg-yellow-400 hover:bg-yellow-500"
                   }`}
                 >
-                  {isSubmitting ? 'Sending' : 'Send'}
+                  {isSubmitting ? "Sending" : "Send"}
                 </button>
               </div>
             </form>
